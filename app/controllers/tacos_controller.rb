d6afd4fe3baf14b729c0080ca67e2c7329a9c43f
G R <gabi.romagnoli@kellogg.northwestern.edu>
2025-02-13 01:33:25 +0000

    before lab 1

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -1,7 +1,8 @@
 class TacosController < ApplicationController
 
   def index
-    render :inline => "<h1>Hello, world!<h1>" #on the application page, which can be found in ports, you can right click and see source code
+    #render :inline => "<h1>Hello, world!<h1>" #on the application page, which can be found in ports, you can right click and see source code
+    render :template => "tacos/index"
   end
 
 end
